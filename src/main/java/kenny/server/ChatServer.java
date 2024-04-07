baf68c8f735b09e868e525527211b32edb0e83fd
kenny <kenny.kwyiu@gmail.com>
2024-04-07 14:39:29 +0800

    7 Apr 2024 implemented addClient() in ChatServer

--- src/main/java/kenny/server/ChatServer.java
+++ src/main/java/kenny/server/ChatServer.java
@@ -1,6 +1,37 @@
 package kenny.server;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChatServer {
     private int DEFAULT_PORT = 8888;
     private final String QUIT = "quit";
+
+    private ServerSocket serverSocket;
+    private Map<Integer, Writer> connectedClients;
+
+    public ChatServer() {
+        connectedClients = new HashMap<Integer, Writer>();
+    }
+
+    public void addClient(Socket socket) throws IOException {
+        if (socket != null) {
+            int port = socket.getPort();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())
+            );
+            connectedClients.put(port, writer);
+            System.out.println("Client[" + port + "] is connected to server");
+        }
+    }
+
+    public void removeClient(Socket socket) {
+
+    }
 }
