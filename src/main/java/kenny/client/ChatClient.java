b9cf77ac4b8225ebab8a4d53baadd07bff0e48aa
kenny <kenny.kwyiu@gmail.com>
2024-04-08 22:09:55 +0800

    8 Apr 2024 implemented send(), receive() and readyToQuit() in ChatClient

--- src/main/java/kenny/client/ChatClient.java
+++ src/main/java/kenny/client/ChatClient.java
@@ -1,7 +1,36 @@
 package kenny.client;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ChatClient {
     private final String DEFAULT_SERVER_HOST = "127.0.0.1";
     private final int DEFAULT_SERVER_PORT = 8888;
     private final String QUIT = "quit";
+
+    private Socket socket;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+
+    public void send(String msg) throws IOException {
+        if (!socket.isOutputShutdown()) {
+            writer.write(msg + "\n");
+            writer.flush();
+        }
+    }
+
+    public String receive() throws IOException {
+        String msg = null;
+        if (!socket.isInputShutdown()) {
+            msg = reader.readLine();
+        }
+        return msg;
+    }
+
+    public boolean readyToQuit(String msg) {
+        return QUIT.equals(msg);
+    }
+
 }
