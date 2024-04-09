7011d182ff4ed1212ade2f5ae2cb4de3b37235c0
kenny <kenny.kwyiu@gmail.com>
2024-04-09 22:25:06 +0800

    9 Apr 2024 implemented UserInputHandler

--- src/main/java/kenny/client/UserInputHandler.java
+++ src/main/java/kenny/client/UserInputHandler.java
@@ -1,4 +1,36 @@
 package kenny.client;
 
-public class UserInputHandler {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UserInputHandler implements Runnable {
+
+    private ChatClient chatClient;
+
+    public UserInputHandler(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    public void run() {
+        try {
+            // waiting for user input
+            BufferedReader consoleReader = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+            while (true) {
+                String input = consoleReader.readLine();
+
+                // send msg to server
+                chatClient.send(input);
+
+                // validate user quit or not
+                if (chatClient.readyToQuit(input)) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
