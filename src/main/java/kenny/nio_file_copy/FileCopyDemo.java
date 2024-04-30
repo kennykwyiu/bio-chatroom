ff150d99d34c426690eb9df99cd70ce17d8f0240
kenny <kenny.kwyiu@gmail.com>
2024-04-30 22:24:29 +0800

    30 Apr 2024 - 6-4 4 noBufferStreamCopy and close()

--- src/main/java/kenny/nio_file_copy/FileCopyDemo.java
+++ src/main/java/kenny/nio_file_copy/FileCopyDemo.java
@@ -1,16 +1,52 @@
 package kenny.nio_file_copy;
 
-import java.io.File;
+import java.io.*;
 
 interface FileCopyRunner {
     void copyFile(File source, File target);
 }
 public class FileCopyDemo {
 
+    private static void close(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        FileCopyRunner noBufferStreamCopy;
+        FileCopyRunner noBufferStreamCopy = new FileCopyRunner() {
+            @Override
+            public void copyFile(File source, File target) {
+                InputStream fileIn = null;
+                OutputStream fileOut = null;
+
+                try {
+                    fileIn = new FileInputStream(source);
+                    fileOut = new FileOutputStream(target);
+
+                    int result;
+                     while ((result = fileIn.read()) != -1) {
+                         fileOut.write(result);
+                     }
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    close(fileIn);
+                    close(fileOut);
+                }
+            }
+        };
+
         FileCopyRunner bufferStreamCopy;
+
         FileCopyRunner nioBufferCopy;
+
         FileCopyRunner noTransferCopy;
     }
 }
