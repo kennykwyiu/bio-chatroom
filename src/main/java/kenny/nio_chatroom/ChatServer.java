a62bd491477667511e15d99a5fb76b4763d9089b
kenny <kenny.kwyiu@gmail.com>
2024-05-04 17:36:44 +0800

    4 May 2024 7-2 added variables

--- src/main/java/kenny/nio_chatroom/ChatServer.java
+++ src/main/java/kenny/nio_chatroom/ChatServer.java
@@ -1,11 +1,23 @@
 package kenny.nio_chatroom;
 
 import java.io.Closeable;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class ChatServer {
     private int DEFAULT_PORT = 8888;
     private final String QUIT = "quit";
     private static final int BUFFER = 1024;
+    private ServerSocketChannel server;
+    private Selector selector;
+    private ByteBuffer readBuffer = ByteBuffer.allocate(BUFFER);
+    private ByteBuffer writeBuffer = ByteBuffer.allocate(BUFFER);
+    private Charset charset = StandardCharsets.UTF_8;
+
     private void Start() {
 
     }
@@ -15,6 +27,13 @@ public class ChatServer {
     }
 
     private void close(Closeable closeable) {
-
+        if (closeable != null) {
+            try {
+                closeable.close();
+                System.out.println("Closing the " + closeable.getClass());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
